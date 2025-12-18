4286854c3b38a67952e73be97b60faae8980ca1a
sixuanli666 <q17317988448@163.com>
2025-12-18 13:23:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1030,7 +1030,7 @@ st.download_button("下载CSV", data=plot_df.reset_index().to_csv(index=False).e
 import pandas as pd
 from PIL import Image
 
-st.markdown("---")
+# st.markdown("---")
 #st.subheader("2.2 行业拥挤度")
 
 # @st.cache_data(show_spinner=False)
